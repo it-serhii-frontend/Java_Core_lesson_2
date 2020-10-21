ceb99a5ae957dea3193baadda67a1e593ca2d96a
Serhii <sergonv077@gmail.com>
2020-10-21 14:36:38 +0200

    addded first project to github

---
+++ lesson1/src/lesson1/lesson11.java
@@ -0,0 +1,28 @@
+package lesson1;
+
+public class lesson11 {
+
+	public static void main(String[] args) {
+	
+	//	byte t=-128;
+	//	short s= 320;
+	//	int i= 50;
+	//	long l= 91;
+	//	char c= "c";
+	//	float f= 1.1f;
+	//  double d= 2.3;
+	//  boolean b= true;
+		
+		Integer a= 5;
+		Double d= 11.4;
+		
+		
+		if (a>d){
+			System.out.println("a>d");
+		} else if (d>a){
+			System.out.println("d>a");
+		} else
+			System.out.println("a=d");
+	}
+
+}
